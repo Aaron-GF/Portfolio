1bf490ae48afc474a4f93b215a2190c303b9fe22
Aaron-GF <aarongarcia567@gmail.com>
2025-05-19 13:10:05 +0200

    refactor: clean up CSS styles for improved layout and responsiveness; update JavaScript for better readability

--- js/main.js
+++ js/main.js
@@ -6,14 +6,14 @@ const copied = document.getElementById('copied');
 const copyMsg = document.querySelector('.tooltip-text')
 
 emailIcon.addEventListener('click', () => {
-    if(email.style.display === 'none' || email.style.display === '') {
+    if (email.style.display === 'none' || email.style.display === '') {
         email.style.display = 'flex';
-        emailIcon.style.background =' #00abf0';
+        emailIcon.style.background = ' #00abf0';
         emailIcon.style.color = '#081b29';
         emailIcon.style.boxShadow = '0 0 10px #00abf0, 0 0 20px rgba(0, 171, 240, 0.3)';
     } else {
         email.style.display = 'none';
-        emailIcon.style.background ='';
+        emailIcon.style.background = '';
         emailIcon.style.color = '';
         emailIcon.style.boxShadow = '';
     }
@@ -21,16 +21,16 @@ emailIcon.addEventListener('click', () => {
 
 copyBtn.addEventListener('click', () => {
     navigator.clipboard.writeText('aarongarcia567@gmail.com')
-    .then(() => {
-        copied.style.display = 'block';
-        copyBtn.style.display = 'none';
-        copyMsg.style.display = 'none';
-        setTimeout(() => {
-            copied.style.display = 'none'
-            copyBtn.style.display = 'block'
-            copyMsg.style.display = 'inline-block'
-        }, 5000)
-    })
+        .then(() => {
+            copied.style.display = 'block';
+            copyBtn.style.display = 'none';
+            copyMsg.style.display = 'none';
+            setTimeout(() => {
+                copied.style.display = 'none'
+                copyBtn.style.display = 'block'
+                copyMsg.style.display = 'inline-block'
+            }, 5000)
+        })
 })
 
 // Reset form after submit ///////////////////////////////////
@@ -42,13 +42,13 @@ form.addEventListener('submit', e => {
     }, 100);
 })
 
-// active menu ////////////////////////////////////
+// active menu on little screens////////////////////////////////////
 const menuLi = document.querySelectorAll('header ul li a');
 const section = document.querySelectorAll('section');
 
 const activeMenu = () => {
     let len = section.length;
-    while(--len && window.scrollY + 97 < section[len].offsetTop) {}
+    while (--len && window.scrollY + 97 < section[len].offsetTop) { }
     menuLi.forEach(sec => sec.classList.remove('active'));
     menuLi[len].classList.add('active');
 };
@@ -59,7 +59,7 @@ window.addEventListener("scroll", activeMenu);
 // sticky navbar ///////////////////////////////////
 const header = document.querySelector('header');
 window.addEventListener('scroll', () => {
-   header.classList.toggle('sticky', window.scrollY > 50) 
+    header.classList.toggle('sticky', window.scrollY > 50)
 })
 
 // icon navbar ///////////////////////////////////
@@ -77,10 +77,9 @@ window.onscroll = () => {
 }
 
 // scroll animations ///////////////////////////////////
-
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        if(entry.isIntersecting) {
+        if (entry.isIntersecting) {
             entry.target.classList.add('show-items');
         } else {
             entry.target.classList.remove('show-items');
@@ -94,3 +93,35 @@ scrollScale.forEach(e => observer.observe(e));
 const scrollBottom = document.querySelectorAll('.scroll-bottom');
 scrollBottom.forEach(e => observer.observe(e));
 
+// touch screen projects section ///////////////////////////////////
+document.addEventListener('DOMContentLoaded', () => {
+    const hoverItems = document.querySelectorAll('.port-box');
+
+    const isTouchDevice = () => {
+        return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+    }
+
+    if (isTouchDevice()) {
+        hoverItems.forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.stopPropagation();
+
+                if (item.classList.contains('active')) {
+                    item.classList.remove('active');
+                } else {
+                    hoverItems.forEach(el => el.classList.remove('active'));
+                    item.classList.add('active');
+                    e.preventDefault();
+                }
+            });
+        });
+
+        document.addEventListener('click', () => {
+            hoverItems.forEach(el => el.classList.remove('active'));
+        });
+
+        window.addEventListener('scroll', () => {
+            hoverItems.forEach(el => el.classList.remove('active'));
+        });
+    }
+});
